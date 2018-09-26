b7f41a17aab5f7aff446916778cd20821c9af649
Kenneth Maguire <kennethmaguire@Kenneths-MacBook-Pro.local>
2018-09-26 13:26:50 -0600

    functioned everything, now have averages working

--- Averages.jl
+++ Averages.jl
@@ -9,11 +9,48 @@ Program 1
 
 =#
 
-
-x = 0;
-
-while x != 100
-    println(x)
-    n = readline()
-    global x = parse(Int, chomp(n))
+function aver(a)
+    i = 1
+    sum = 0
+    average = 0
+    println("the list is")
+    while i <= length(a)
+        println(a[i])
+        sum += a[i]
+        i += 1
+    end
+    println("The sum is: " * string(sum))
+    average = (sum/length(a))
+    println("The average is: " * string(average))
 end
+
+
+function askForInput(_inputInt)
+    print("\n")
+    _listLen = Int64[]
+    i = 1
+    println("Please enter " * string(_inputInt) * " integers")
+    while  i <= _inputInt
+        i += 1
+        n = readline()      #read string value from term
+        x = parse(Int, chomp(n)) #convert string to int
+        push!(_listLen, x)       #append int to end of the array
+    end
+    return _listLen
+end
+
+
+println("Please enter the number of integers you'd like to average")
+
+inputStr = readline()
+inputInt = parse(Int, chomp(inputStr))
+
+
+
+print("\n")
+#println(length(listLen))
+listLen = askForInput(inputInt)
+
+aver(listLen)
+
+println(listLen)
